90dc57210e8429b942c470543ad94286a1fb7766
Ptashenko Maxim <theykropchik@gmail.com>
2022-12-05 18:00:05 +0400

    git committed

---
+++ main.js
@@ -0,0 +1,26 @@
+const alphavit = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numAlph = {}
+
+for( let i = 0; i < alphavit.length; i++){
+    numAlph[alphavit[i]] = i;
+}
+
+function Encode(){
+    let text = document.getElementById("word").value;
+    let key = document.getElementById("key").value;
+    let code = '';
+    for(let i = 0; i < text.length; i++){
+        code += alphavit[(numAlph[text[i]]+numAlph[key[i%key.length]])%alphavit.length];
+    }
+    alert(code);
+}
+function Decode(){
+    let text = document.getElementById("deword").value;
+    let key =document.getElementById("dekey").value;
+
+    let code = '';
+    for(let i = 0; i < text.length; i++){
+    code += alphavit[(numAlph[text[i]]-numAlph[key[i%key.length]]+alphavit.length)%alphavit.length];
+    }
+    alert(code);
+}
