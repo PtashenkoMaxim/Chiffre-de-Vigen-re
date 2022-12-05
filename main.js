430f2fe45d51502ab156022b01fbe687efadfb41
Ptashenko Maxim <theykropchik@gmail.com>
2022-12-05 18:46:50 +0400

    add class

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
-const alphavit = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numAlph = {}
+class Algoritm {
+    constructor(word, key) {
+        this.word = word;
+        this.key = key;
+      }
 
-for( let i = 0; i < alphavit.length; i++){
-    numAlph[alphavit[i]] = i;
+    EncDecode(isEncode){
+        const alphavit = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя "
+        const numAlph = {}
+        for( let i = 0; i < alphavit.length; i++){
+            numAlph[alphavit[i]] = i;
+        }
+        let code = '';
+        if (isEncode == true) {
+            for(let i = 0; i < this.word.length; i++){
+                code += alphavit[(numAlph[this.word[i]]+numAlph[this.key[i%this.key.length]])%alphavit.length];
+            }
+            return code;
+        } else {
+            let code = '';
+            for(let i = 0; i < this.word.length; i++){
+            code += alphavit[(numAlph[this.word[i]]-numAlph[this.key[i%this.key.length]]+alphavit.length)%alphavit.length];
+            }
+            return code;
+        }
+    }
 }
 
 function Encode(){
     let text = document.getElementById("word").value;
     let key = document.getElementById("key").value;
-    let code = '';
-    for(let i = 0; i < text.length; i++){
-        code += alphavit[(numAlph[text[i]]+numAlph[key[i%key.length]])%alphavit.length];
-    }
+    const result = new Algoritm(text, key)
+    let code = result.EncDecode(true);
+    console.log(result)
     alert(code);
 }
 function Decode(){
     let text = document.getElementById("deword").value;
     let key =document.getElementById("dekey").value;
-
-    let code = '';
-    for(let i = 0; i < text.length; i++){
-    code += alphavit[(numAlph[text[i]]-numAlph[key[i%key.length]]+alphavit.length)%alphavit.length];
-    }
+    const result = new Algoritm(text, key)
+    let code = result.EncDecode(false);
     alert(code);
 }
-неужели понял
+
